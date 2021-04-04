df55d984e09d3df4f018b0dc85a0a2f81be4ee5a
Štěpán Śevčík <Thoronir42@Gmail.com>
2021-04-04 15:03:54 +0200

    [app] Improve Home and other common visuals

--- src/modules/app/views/Home.vue
+++ src/modules/app/views/Home.vue
@@ -1,23 +1,25 @@
 <template>
   <div class="home">
     <template v-if="!userAuthenticated">
-      <p>
-        Vítejte v aplikaci {{ appName }} - aplikaci poskytující základní správu vašich šelem a možnosti plánování
-        připouštění.
-        Více se můžete dočíst na stránce
-        <router-link :to="{name: 'app.About'}">{{ $t('app.view.About') }}</router-link>
-        .
-      </p>
-      <p>
-        Aktuálně nemáte uložena žádná data. Můžete se
-        <router-link :to="{name: 'auth.SignIn'}">přihlásit</router-link>
-        anebo
-        vyzkoušet <a href="#" @click.prevent="beginDemo">demo účet</a>.
+      <p>Vítejte v aplikace {{ appName }}. Nyní zde nemáte uloženy žádné údaje.</p>
+
+      <div class="start-actions">
+        <router-link :to="{name: 'auth.SignIn'}" class="btn btn-primary">Připravit prázdná data</router-link>
+        <button class="btn btn-secondary" @click.prevent="beginDemo">Načíst demo data</button>
+      </div>
+
+      <p class="mt-5">
+        Více se můžete dočíst na stránce <router-link :to="{name: 'app.About'}">{{ $t('app.view.About') }}</router-link>.
       </p>
     </template>
+
     <template v-else>
       <p>
-        Vítejte zpět v aplikaci {{ appName }}. Počet evidovaných šelem: <b>{{ beastSummary.count }}</b>.
+        Vítejte zpět v aplikaci {{ appName }}.
+      </p>
+      <p>
+        Právě máte uloženo <router-link :to="{name: 'bestiary.MyBeasts'}">{{ bestiarySummary.beastCount }} šelem</router-link>
+        v <router-link :to="{name: 'bestiary.BreedingStationsIndex'}">{{ bestiarySummary.stationCount }} stanicích</router-link>.
       </p>
     </template>
   </div>
@@ -37,8 +39,9 @@ export default defineComponent({
     translateMixin,
   ],
   setup() {
-    const beastSummary = reactive({
-      count: computed(() => beastsStore.getters.count())
+    const bestiarySummary = reactive({
+      beastCount: computed(() => beastsStore.state.beastList.length),
+      stationCount: computed(() => beastsStore.state.breedingStations.length)
     })
     const userAuthenticated = computed(() => authStore.getters.isLoggedIn())
 
@@ -48,8 +51,27 @@ export default defineComponent({
       beginDemo: () => {
         demoStore.actions.initDemoData()
       },
-      beastSummary,
+      bestiarySummary,
     }
   },
 });
 </script>
+
+<style lang="scss">
+.start-actions {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  .btn-primary {
+    padding: 1em;
+    font-size: 24pt;
+  }
+
+  .btn-secondary {
+    margin-top: 0.5em;
+
+    padding: 0.25em 1em;
+  }
+}
+</style>
--- src/modules/typeful/components/DecForm.vue
+++ src/modules/typeful/components/DecForm.vue
@@ -2,20 +2,25 @@
   <form @submit.prevent="onSubmit(modelObj)">
     <slot/>
 
-    <div v-if="onSubmit" class="form-group">
-      <button class="btn btn-primary" type="submit">Yeeee</button>
+    <div v-if="onSubmit" class="form-group text-center">
+      <button class="btn btn-primary" type="submit">{{ submitText || $t('app.generic.save') }}</button>
     </div>
   </form>
 </template>
 
 <script>
 import {computed, provide, ref} from "vue";
+import {translateMixin} from "@/i18n";
 
 export default {
   name: "DecForm",
+  mixins: [
+    translateMixin,
+  ],
   props: {
     modelValue: {type: Object},
     onSubmit: {type: Function},
+    submitText: {type: String},
   },
   emits: {
     'success'() {
