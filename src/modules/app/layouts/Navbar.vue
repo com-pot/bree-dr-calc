8908b8fea85c5b894c724f8d4e0373138857c8f0
Štěpán Śevčík <Thoronir42@Gmail.com>
2021-04-04 16:21:23 +0200

    [app] Use more sane layout components

--- src/modules/app/layouts/Navbar.vue
+++ src/modules/app/layouts/Navbar.vue
@@ -1,14 +1,29 @@
 <template>
-  <div v-if="navLinks" id="navbar">
-    <router-link class="brand" to="/">{{ appName }}</router-link>
+  <nav class="navbar sticky-top navbar-expand navbar-light bg-light" id="navbar">
+    <div class="container-fluid">
+      <router-link class="navbar-brand" to="/">{{ appName }}</router-link>
 
-    <nav>
-      <template v-for="(link, i) in navLinks" :key="i">
-        <router-link :to="link.to">{{ link.text }}</router-link>
-        <i v-if="i < navLinks.length - 1" class="spacer"></i>
-      </template>
-    </nav>
-  </div>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+              aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav ml-auto">
+          <template v-for="(link, i) in navLinks" :key="i">
+            <li class="nav-item">
+              <router-link :to="link.to" class="nav-link" active-class="active"
+                           aria-current-value="page">{{ link.text }}</router-link>
+            </li>
+            <i v-if="i < navLinks.length - 1" class="spacer"></i>
+          </template>
+<!--          <li class="nav-item">
+            <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+          </li>-->
+        </ul>
+      </div>
+    </div>
+  </nav>
 </template>
 
 <script>
